f02709e7f91599d1fd59e0d9265fde243f4abef2
Kalidoss Anandh <kiyotaka444@duck.com>
2025-06-13 11:47:07 +0530

    deployment config

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -8,10 +9,19 @@ const noteRoutes = require('./routes/noteRoutes');
 const connectDB = require('./config/db');
 
 connectDB();
-app.use(cors({ origin: 'http://localhost:2005' }));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({ origin: 'http://localhost:2005' }));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/notes', noteRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 app.all(/(.*)/, (req, res) => {
   res.status(404).send('The requested page does not exist.');
